c5262f5d9534c221ae607cab566f8cc4cbe60b5f
Lorenzo <lorenzo8banachewicz@gmail.com>
2026-07-13 10:59:50 +0200

    Array object Map

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,15 @@
-import Header from "./Header";
-import Hero from "./Hero";
-import Sidebar from "./Sidebar";
-import Footer from "./Footer";
-
 function App(){
+  const students = ["Rahim", "Karim", "Jamal", "bob"];
+
   return(
-    <>
     <div>
-      <Header
-      title="Lorenzo's Portfolio"
-      nav1="Home"
-      nav2="About me"
-      nav3="Studies"
-      nav4="Contact"
-      />
-
-      <Hero
-        headline="About me"
-        description="Hi im Lorenzo, im deeply interested in programming"
-      />
-
-      <Sidebar
-      title="Contact"
-      phone="607922395"
-      email="lorenzo8banachewicz@gmail.com"
-      />
-
-      <Footer
-      text="@ 2026 Lorenzo"
-      />
-      
-      
-
+      {students.map((student) => (
+        <h2>{student}</h2>
+      ))}
     </div>
-    </>
+   
   );
+   
 }
 
 export default App;
-
-
