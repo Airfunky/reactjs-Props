5231742955825318ae06a97b89f92ad8495bf7de
Lorenzo <lorenzo8banachewicz@gmail.com>
2026-07-14 13:02:34 +0200

    Conditional Method 1, if statement

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,14 @@ import { useState } from "react";
 
 function App() {
  
- const [text, setText] = useState("");
+const isLogin = true;
 
-  return (
-    <div>
-      <h1>Character Counter</h1>
-      <input type="text"
-      placeholder="Enter Your text"
-      onChange={(e) => setText(e.target.value)} />
-      <h2> {text}</h2>
-      <p>Characters: {text.length}</p>
-    </div>
- 
-  );
+if (isLogin){
+  return <h1>Welcome Back!</h1>;
+}
+
+  return <h1>Please Login</h1>;
+   
 }
 
 export default App;
