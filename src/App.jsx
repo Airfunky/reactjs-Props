dae7583fe20bce8e35c16e85782596c5824369ab
Lorenzo <lorenzo8banachewicz@gmail.com>
2026-07-14 10:54:38 +0200

    useState beginnings

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ function App() {
     <div>
       <h1>{count}</h1>
 
-      <button 
-        onClick={() => setCount(count + 1) }
-        >
-          Increase
-        </button>
+      <button onClick={() => setCount(count + 1) }> Increase </button>
+
+      <button onClick={() => setCount(count - 1) }> Decrease </button>
+
+      <button onClick={() => setCount(0) }> Reset </button>
     </div>
   );
 }
